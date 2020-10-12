6fac92a9411823158f591b8a0a35a1f285f19697
Ivan Terziev <ivanterziev38@gmail.com>
2020-10-12 16:33:27 +0300

    Initial Commit

---
+++ Auto Music Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Music Player
+//
+//  Created by Ivan Terziev on 12.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
